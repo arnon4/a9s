e3a53096b84a9fdbfe394242ded9854314a942d5
Arnon <98772127+arnon4@users.noreply.github.com>
2026-07-17 12:16:30 +0300

    First release

--- build.zig
+++ build.zig
@@ -2,35 +2,25 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const optimize = std.builtin.OptimizeMode.ReleaseSmall;
+    const target = b.standardTargetOptions(.{});
 
-    const targets: []const std.Target.Query = &.{
-        .{ .cpu_arch = .x86_64, .os_tag = .windows, .abi = .gnu },
-        .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
-        // TODO: add Mac
-    };
-
-    for (targets) |t| {
-        const target = b.resolveTargetQuery(t);
-        const exe = b.addExecutable(.{
-            .name = "a9s",
-            .root_module = b.createModule(.{
-                .root_source_file = b.path("src/main.zig"),
-                .target = target,
-                .optimize = optimize,
-            }),
-        });
-        if (t.os_tag == .macos) {
-            exe.root_module.link_libc = true;
-        }
-        b.installArtifact(exe);
+    const exe = b.addExecutable(.{
+        .name = "a9s",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+    if (target.result.os.tag == .macos) {
+        exe.root_module.link_libc = true;
     }
+    b.installArtifact(exe);
 
-    // Tests run against the native target
-    const native_target = b.standardTargetOptions(.{});
     const exe_tests = b.addTest(.{
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
-            .target = native_target,
+            .target = target,
             .optimize = optimize,
         }),
     });
